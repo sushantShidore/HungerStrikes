29177e333686e458801a003204d1aaf55b47ad9f
Sushant Shidore <sushant.shidore,social@gmail.com>
2019-01-01 18:57:59 +0530

    Committing first version

---
+++ HungerStrike_FileNameProcessor/FileNameProcessor/FileNameProcessor/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace FileNameProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_EnterFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_StartDayDate = new System.Windows.Forms.TextBox();
+            this.textBox_StartDayMonth = new System.Windows.Forms.TextBox();
+            this.textBox_StartDayYear = new System.Windows.Forms.TextBox();
+            this.textBox_EndDayYear = new System.Windows.Forms.TextBox();
+            this.textBox_EndDayMonth = new System.Windows.Forms.TextBox();
+            this.textBox_EndDayDate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_Debug = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_EnterFileName
+            // 
+            this.textBox_EnterFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_EnterFileName.Location = new System.Drawing.Point(13, 55);
+            this.textBox_EnterFileName.Name = "textBox_EnterFileName";
+            this.textBox_EnterFileName.Size = new System.Drawing.Size(306, 31);
+            this.textBox_EnterFileName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Type in file name:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(307, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Extract Dates";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Start Day Date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Start Day Month:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Start Day Year:";
+            // 
+            // textBox_StartDayDate
+            // 
+            this.textBox_StartDayDate.BackColor = System.Drawing.Color.White;
+            this.textBox_StartDayDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_StartDayDate.Location = new System.Drawing.Point(182, 155);
+            this.textBox_StartDayDate.Name = "textBox_StartDayDate";
+            this.textBox_StartDayDate.ReadOnly = true;
+            this.textBox_StartDayDate.Size = new System.Drawing.Size(138, 31);
+            this.textBox_StartDayDate.TabIndex = 6;
+            // 
+            // textBox_StartDayMonth
+            // 
+            this.textBox_StartDayMonth.BackColor = System.Drawing.Color.White;
+            this.textBox_StartDayMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_StartDayMonth.Location = new System.Drawing.Point(182, 193);
+            this.textBox_StartDayMonth.Name = "textBox_StartDayMonth";
+            this.textBox_StartDayMonth.ReadOnly = true;
+            this.textBox_StartDayMonth.Size = new System.Drawing.Size(138, 31);
+            this.textBox_StartDayMonth.TabIndex = 7;
+            // 
+            // textBox_StartDayYear
+            // 
+            this.textBox_StartDayYear.BackColor = System.Drawing.Color.White;
+            this.textBox_StartDayYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_StartDayYear.Location = new System.Drawing.Point(182, 231);
+            this.textBox_StartDayYear.Name = "textBox_StartDayYear";
+            this.textBox_StartDayYear.ReadOnly = true;
+            this.textBox_StartDayYear.Size = new System.Drawing.Size(138, 31);
+            this.textBox_StartDayYear.TabIndex = 8;
+            // 
+            // textBox_EndDayYear
+            // 
+            this.textBox_EndDayYear.BackColor = System.Drawing.Color.White;
+            this.textBox_EndDayYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_EndDayYear.Location = new System.Drawing.Point(182, 373);
+            this.textBox_EndDayYear.Name = "textBox_EndDayYear";
+            this.textBox_EndDayYear.ReadOnly = true;
+            this.textBox_EndDayYear.Size = new System.Drawing.Size(138, 31);
+            this.textBox_EndDayYear.TabIndex = 14;
+            // 
+            // textBox_EndDayMonth
+            // 
+            this.textBox_EndDayMonth.BackColor = System.Drawing.Color.White;
+            this.textBox_EndDayMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_EndDayMonth.Location = new System.Drawing.Point(182, 335);
+            this.textBox_EndDayMonth.Name = "textBox_EndDayMonth";
+            this.textBox_EndDayMonth.ReadOnly = true;
+            this.textBox_EndDayMonth.Size = new System.Drawing.Size(138, 31);
+            this.textBox_EndDayMonth.TabIndex = 13;
+            // 
+            // textBox_EndDayDate
+            // 
+            this.textBox_EndDayDate.BackColor = System.Drawing.Color.White;
+            this.textBox_EndDayDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_EndDayDate.Location = new System.Drawing.Point(182, 297);
+            this.textBox_EndDayDate.Name = "textBox_EndDayDate";
+            this.textBox_EndDayDate.ReadOnly = true;
+            this.textBox_EndDayDate.Size = new System.Drawing.Size(138, 31);
+            this.textBox_EndDayDate.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 378);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "End Day Year:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 340);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "End Day Month:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(15, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "End Day Date:";
+            // 
+            // textBox_Debug
+            // 
+            this.textBox_Debug.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Debug.Location = new System.Drawing.Point(13, 428);
+            this.textBox_Debug.Name = "textBox_Debug";
+            this.textBox_Debug.Size = new System.Drawing.Size(306, 31);
+            this.textBox_Debug.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 476);
+            this.Controls.Add(this.textBox_Debug);
+            this.Controls.Add(this.textBox_EndDayYear);
+            this.Controls.Add(this.textBox_EndDayMonth);
+            this.Controls.Add(this.textBox_EndDayDate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox_StartDayYear);
+            this.Controls.Add(this.textBox_StartDayMonth);
+            this.Controls.Add(this.textBox_StartDayDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_EnterFileName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "HungerStrikes: Menu File Name Processor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_EnterFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_StartDayDate;
+        private System.Windows.Forms.TextBox textBox_StartDayMonth;
+        private System.Windows.Forms.TextBox textBox_StartDayYear;
+        private System.Windows.Forms.TextBox textBox_EndDayYear;
+        private System.Windows.Forms.TextBox textBox_EndDayMonth;
+        private System.Windows.Forms.TextBox textBox_EndDayDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_Debug;
+    }
+}
+
---
+++ HungerStrike_FileNameProcessor/FileNameProcessor/FileNameProcessor/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileNameProcessor
+{
+    public partial class Form1 : Form
+    {
+        DateTime today;
+        DateTime weekStartDay;
+        DateTime weekEndDay;
+
+        int startDayDate;
+        int startDayMonth;
+        int startDayYear;
+
+        int endDayDate;
+        int endDayMonth;
+        int endDayYear;
+
+        string[] monthNames = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            today = DateTime.Today;
+            textBox_Debug.Text = today.ToString("dd-mm-yyyy");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileNameString = textBox_EnterFileName.Text;
+
+            textBox_Debug.Text = fileNameString;
+
+            string[] numbers = Regex.Split(fileNameString, @"\D+");
+            string[] strings = Regex.Split(fileNameString, @"\d+");
+
+            //It is observed that sometimes empty elements are added to the extracted array of strings while
+            //performing regex. Check if such empty elements exist, and smoke'em off if present.
+            //Solution Credit: https://stackoverflow.com/questions/8814811/remove-blank-values-in-the-array-using-c-sharp
+
+            var temp = new List<string>();
+            foreach (var s in numbers)
+            {
+                if (!string.IsNullOrEmpty(s))
+                    temp.Add(s);
+            }
+            numbers = temp.ToArray();
+
+            //>>>>Step 1: Find Date and Year of Starting Weekday and Ending Weekday
+            //This can be identified by counting number of occurrences of numbers in the file name string.
+            //If the filename contains 2 numbers, 
+            //then first number is start date, second is end date. Year is to be assigned using current year.
+            //
+            //If the filename contains 3 numbers, 
+            //then first number is start date, second is end date and third is year same for start and end date
+            //
+            //If the filename contains 4 numbers,
+            //then first number is start date, second is year of the start date, third is end date and last is yesr for end date.
+            //
+            //This is assumming that (and putting a restriction on) naming the file should contain the month in string format
+            //like 'Jan', 'Apr', 'Sept' and not in numeric format like '1', '4', '9'.
+            //
+            //IF THIS IS NOT FOLLOWED, THEN ALL THIS ALGORITHM WILL FAIL.
+            if ((numbers.Length >= 2) && (numbers.Length <= 4))
+            {
+                //First element numbers[0] has been extracted successfully.
+                if (numbers.Length == 2u)
+                {
+                    //where [0] element is date of starting weekday
+                    //      [1] element is date of ending weekday
+                    startDayDate    = int.Parse(numbers[0]);
+                    endDayDate      = int.Parse(numbers[1]);
+
+                    //Since year is not mentioned in the file name, simply assume and assign the current year
+                    //to both the dates.
+                    startDayYear    = today.Year;
+                    endDayYear      = today.Year;
+                }
+                else if (numbers.Length == 3u)
+                {
+                    //where [0] element is date of starting weekday
+                    //      [1] element is date of ending weekday
+                    //      [2] element is year of starting and ending weekdays
+                    startDayDate    = int.Parse(numbers[0]);
+                    endDayDate      = int.Parse(numbers[1]);
+                    startDayYear    = int.Parse(numbers[2]);
+                    endDayYear      = int.Parse(numbers[2]);
+                }
+                else
+                {
+                    //where [0] element is date of starting weekday
+                    //      [1] element is year of starting weekday
+                    //      [2] element is date of ending weekday
+                    //      [3] element is year of ending weekday
+                    startDayDate    = int.Parse(numbers[0]);
+                    startDayYear    = int.Parse(numbers[1]);
+                    endDayDate      = int.Parse(numbers[2]);
+                    endDayYear      = int.Parse(numbers[3]);
+                }
+
+                textBox_StartDayDate.Text   = startDayDate.ToString();
+                textBox_StartDayYear.Text   = startDayYear.ToString();
+
+                textBox_EndDayDate.Text     = endDayDate.ToString();
+                textBox_EndDayYear.Text     = endDayYear.ToString();
+
+            }
+            else
+            {
+                //Invalid number of occurrences of numbers found in filename
+                //TODO Exception Handling
+                textBox_Debug.Text = "Dates invalid";
+            }
+
+            //>>>>Step 2: Find Start Month
+            //Fetch names of the month appearing in the file name.
+            //If it's just one, then month is same for start and end week days.
+            //If there're two, then the months are different in start and end week days, meaning a month crossover in the week.
+            List<int> monthIndicesFound = new List<int>();
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if(strings[i] != "")
+                {
+                    for(int j=0; j < monthNames.Length; j++)
+                    {
+                        //if (strings[i].Contains(monthNames[j], StringComparer.OrdinalIgnoreCase))
+                        if(strings[i].IndexOf(monthNames[j], StringComparison.OrdinalIgnoreCase) > 0 )
+                        {
+                            monthIndicesFound.Add((j+1));
+                        }
+                    }
+                }
+            }
+
+            if ( (monthIndicesFound.Count > 0) && (monthIndicesFound.Count < 3))
+            {
+                if (monthIndicesFound.Count == 1)
+                {
+                    startDayMonth = monthIndicesFound[0];
+                    endDayMonth = monthIndicesFound[0];
+                }
+                else
+                {
+                    startDayMonth = monthIndicesFound[0];
+                    endDayMonth = monthIndicesFound[1];
+                }
+
+                textBox_StartDayMonth.Text   = startDayMonth.ToString();
+                textBox_EndDayMonth.Text     = endDayMonth.ToString();
+            }
+            else
+            {
+                //Invalid number of months found in file name.
+                //TODO Exception Handling
+                textBox_Debug.Text = "Dates invalid";
+            }
+
+            if(validateDates() == true)
+            {
+                textBox_Debug.Text = "Dates valid";
+            }
+            else
+            {
+                textBox_Debug.Text = "Dates invalid";
+            }
+
+            //foreach (string value in numbers)
+            //{
+            //    if (!string.IsNullOrEmpty(value))
+            //    {
+            //        int i = int.Parse(value);
+            //        Console.WriteLine("Number: {0}", i);
+            //    }
+            //}
+        }
+
+        private bool validateDates()
+        {
+            bool result = true;
+
+            //Check if year is in 2 digit for. Convert in 4 digit if required.
+            if (startDayYear - 2000 < 0)
+            {
+                startDayYear = startDayYear + 2000;
+            }
+
+            if (endDayYear - 2000 < 0)
+            {
+                endDayYear = endDayYear + 2000;
+            }
+
+            if(startDayYear > endDayYear)
+            {
+                result = false;
+            }
+            else
+            {
+                if ((startDayMonth == 0) || (startDayMonth > 12) || (endDayMonth == 0) || (endDayMonth > 12))
+                {
+                    result = false;
+                }
+                else
+                {
+                    if((startDayYear == endDayYear) && (startDayMonth > endDayMonth))
+                    {
+                        result = false;
+                    }
+                    else
+                    {
+                        if((startDayYear == endDayYear) && (startDayMonth == endDayMonth) && (startDayDate >= endDayDate))
+                        {
+                            result = false;
+                        }
+                        else
+                        {
+                            if( (startDayDate == 0) || (endDayDate == 0) || (startDayDate > 31) || (endDayDate > 31) )
+                            {
+                                result = false;
+                            }
+                            else
+                            {
+                                if ( (startDayMonth == 4) || (startDayMonth == 6) || (startDayMonth == 9) || (startDayMonth == 11) )
+                                {
+                                    if (startDayDate > 30)
+                                    {
+                                        result = false;
+                                    }
+                                }
+                                else
+                                {
+                                    if( (startDayYear %4 != 0) && (startDayMonth == 2) && (startDayDate > 28) )
+                                    {
+                                        result = false;
+                                    }
+                                }
+
+                                if ( (endDayMonth == 4) || (endDayMonth == 6) || (endDayMonth == 9) || (endDayMonth == 11) )
+                                {
+                                    if (endDayDate > 30)
+                                    {
+                                        result = false;
+                                    }
+                                }
+                                else
+                                {
+                                    if ((endDayYear % 4 != 0) && (endDayMonth == 2) && (endDayDate > 28))
+                                    {
+                                        result = false;
+                                    }
+                                }
+
+                                //Final test - check if the start date and end date cover one entire week
+                                if (result == true)
+                                {
+                                    weekStartDay = new DateTime(startDayYear, startDayMonth, startDayDate);
+                                    weekEndDay = new DateTime(endDayYear, endDayMonth, endDayDate);
+
+                                    if ((weekEndDay - weekStartDay).Days != 4)
+                                    {
+                                        result = false;
+                                    }
+                                    else
+                                    {
+                                        //Phew! All test passed.
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
